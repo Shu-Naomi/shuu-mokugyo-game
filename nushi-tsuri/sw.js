23d514a0380bf28acf5c868fd5346e8a0f95ea9b
Shu-Naomi <grey2291720@gmail.com>
2026-07-13 00:48:29 +0900

    ぬし釣りドット素材をオフライン対象へ追加

--- nushi-tsuri/sw.js
+++ nushi-tsuri/sw.js
@@ -1,11 +1,43 @@
-const CACHE_NAME = "nushi-tsuri-v2-landscape";
-const APP_ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_ASSETS)).then(() => self.skipWaiting()));
+const CACHE_NAME = "nushi-tsuri-v3-town";
+const APP_ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./assets/riku-walk.png",
+  "./assets/fish-shadows.png",
+];
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_ASSETS))
+      .then(() => self.skipWaiting()),
+  );
 });
-self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys
+            .filter((key) => key !== CACHE_NAME)
+            .map((key) => caches.delete(key)),
+        ),
+      )
+      .then(() => self.clients.claim()),
+  );
 });
-self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).catch(() => caches.match("./index.html"))));
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(
+        (cached) =>
+          cached ||
+          fetch(event.request).catch(() => caches.match("./index.html")),
+      ),
+  );
 });
