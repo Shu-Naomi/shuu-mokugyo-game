a437f99e8226fbcc1ed65eb3c71e6ad416c110b0
Shu-Naomi <grey2291720@gmail.com>
2026-07-12 23:30:41 +0900

    ぬし釣り横画面版のキャッシュを更新

--- nushi-tsuri/sw.js
+++ nushi-tsuri/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nushi-tsuri-v1";
+const CACHE_NAME = "nushi-tsuri-v2-landscape";
 const APP_ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_ASSETS)).then(() => self.skipWaiting()));
