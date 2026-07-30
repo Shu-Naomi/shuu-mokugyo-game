a57d5475938fa94d9238457b2a3fd11bfe461a14
Shu-Naomi <grey2291720@gmail.com>
2026-07-31 04:17:52 +0900

    Build v46 watershed field

--- nushi-tsuri/sw.js
+++ nushi-tsuri/sw.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = "nushi-tsuri-v45-pixel-terrain-1";
+const CACHE_NAME = "nushi-tsuri-v46-watershed-1";
 const CORE_ASSETS = [
   "./",
   "./index.html",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png",
+  "./assets/terrain-world-v46.svg",
   "./assets/terrain-water-v45.svg",
   "./assets/terrain-grass-v45.svg",
   "./assets/terrain-sand-v45.svg",
