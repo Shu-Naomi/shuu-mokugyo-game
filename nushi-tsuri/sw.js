2714ce780eda1fc3c589bfe1815e73308e86a26e
Shu-Naomi <grey2291720@gmail.com>
2026-07-31 20:38:41 +0900

    Remove visible terrain tiling in v48

--- nushi-tsuri/sw.js
+++ nushi-tsuri/sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "nushi-tsuri-v47-fine-pixel-1";
+const CACHE_NAME = "nushi-tsuri-v48-nonrepeating-1";
 const CORE_ASSETS = [
   "./",
   "./index.html",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png",
-  "./assets/terrain-world-v47.svg",
+  "./assets/terrain-world-v48.png",
   "./assets/terrain-water-v45.svg",
   "./assets/terrain-grass-v45.svg",
   "./assets/terrain-sand-v45.svg",
