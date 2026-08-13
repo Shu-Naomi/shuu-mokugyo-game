4114cdcdaef066d5ee385760a7d66d116b71e6d6
Shu-Naomi <grey2291720@gmail.com>
2026-08-14 06:18:24 +0900

    Add memory game constants

--- memory/game.js
+++ memory/game.js
@@ -1,6 +1,6 @@
 'use strict';
 const icons=['🍘','🦴','🐟','🎾','🪵','🐾'];
-let cards=[];
-let turn='human';
+const skill={easy:.35,normal:.65,hard:.9};
+let diff='normal',cards=[],open=[],turn=0,humanScore=0,shuuScore=0;
 function shuffle(values){return [...values].sort(()=>Math.random()-.5);}
 console.log('犬小屋神経衰弱 ready',shuffle(icons));
