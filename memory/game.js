51170e92ed497a206a009bfb97bdfdd2f30ffe46
Shu-Naomi <grey2291720@gmail.com>
2026-08-25 04:26:30 +0900

    Remove obsolete memory scaffold script

--- memory/game.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const icons=['🍘','🦴','🐟','🎾','🪵','🐾'];
-const skill={easy:.35,normal:.65,hard:.9};
-let diff='normal',cards=[],open=[],turn=0,humanScore=0,shuuScore=0;
-function shuffle(values){return [...values].sort(()=>Math.random()-.5);}
-console.log('犬小屋神経衰弱 ready',shuffle(icons));
